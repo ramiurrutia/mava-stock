46686e8e98ba2c0e987154b45ac78244f2ad3647
Ramiro Urrutia <urrutiarami@gmail.com>
2026-06-26 01:04:35 -0300

    Refactor code structure and remove redundant changes

--- data/products.ts
+++ data/products.ts
@@ -31,6 +31,119 @@ import xg006 from "@/app/sources/images/xg/XG-006.png";
 import xg007 from "@/app/sources/images/xg/XG-007.png";
 import xg008 from "@/app/sources/images/xg/XG-008.png";
 import xg009 from "@/app/sources/images/xg/XG-009.png";
+import xg010 from "@/app/sources/images/xg/XG-010.png";
+import xg011 from "@/app/sources/images/xg/XG-011.png";
+import xg012 from "@/app/sources/images/xg/XG-012.png";
+import xg013 from "@/app/sources/images/xg/XG-013.png";
+import xg014 from "@/app/sources/images/xg/XG-014.png";
+import xg015 from "@/app/sources/images/xg/XG-015.png";
+import xg016 from "@/app/sources/images/xg/XG-016.png";
+import xg017 from "@/app/sources/images/xg/XG-017.png";
+import xg018 from "@/app/sources/images/xg/XG-018.png";
+import xg019 from "@/app/sources/images/xg/XG-019.png";
+import xg020 from "@/app/sources/images/xg/XG-020.png";
+import xg021 from "@/app/sources/images/xg/XG-021.png";
+import xg022 from "@/app/sources/images/xg/XG-022.png";
+import xg023 from "@/app/sources/images/xg/XG-023.png";
+import xg024 from "@/app/sources/images/xg/XG-024.png";
+import xg025 from "@/app/sources/images/xg/XG-025.png";
+import xg026 from "@/app/sources/images/xg/XG-026.png";
+import xg027 from "@/app/sources/images/xg/XG-027.png";
+import xg028 from "@/app/sources/images/xg/XG-028.png";
+import xg029 from "@/app/sources/images/xg/XG-029.png";
+import xg030 from "@/app/sources/images/xg/XG-030.png";
+import xg031 from "@/app/sources/images/xg/XG-031.png";
+import xg032 from "@/app/sources/images/xg/XG-032.png";
+import xg033 from "@/app/sources/images/xg/XG-033.png";
+import xg034 from "@/app/sources/images/xg/XG-034.png";
+import xg035 from "@/app/sources/images/xg/XG-035.png";
+import xg036 from "@/app/sources/images/xg/XG-036.png";
+import xg037 from "@/app/sources/images/xg/XG-037.png";
+import xg038 from "@/app/sources/images/xg/XG-038.png";
+import xg039 from "@/app/sources/images/xg/XG-039.png";
+import xg040 from "@/app/sources/images/xg/XG-040.png";
+import xg041 from "@/app/sources/images/xg/XG-041.png";
+import xg042 from "@/app/sources/images/xg/XG-042.png";
+import xg043 from "@/app/sources/images/xg/XG-043.png";
+import xg044 from "@/app/sources/images/xg/XG-044.png";
+import xg045 from "@/app/sources/images/xg/XG-045.png";
+import xg046 from "@/app/sources/images/xg/XG-046.png";
+import xg047 from "@/app/sources/images/xg/XG-047.png";
+import xg048 from "@/app/sources/images/xg/XG-048.png";
+import xg049 from "@/app/sources/images/xg/XG-049.png";
+import xg050 from "@/app/sources/images/xg/XG-050.png";
+import xg051 from "@/app/sources/images/xg/XG-051.png";
+import xg052 from "@/app/sources/images/xg/XG-052.png";
+import xg053 from "@/app/sources/images/xg/XG-053.png";
+import xg054 from "@/app/sources/images/xg/XG-054.png";
+import xg055 from "@/app/sources/images/xg/XG-055.png";
+import xg056 from "@/app/sources/images/xg/XG-056.png";
+import xg057 from "@/app/sources/images/xg/XG-057.png";
+import xg058 from "@/app/sources/images/xg/XG-058.png";
+import xg059 from "@/app/sources/images/xg/XG-059.png";
+import xg060 from "@/app/sources/images/xg/XG-060.png";
+import xg061 from "@/app/sources/images/xg/XG-061.png";
+import xg062 from "@/app/sources/images/xg/XG-062.png";
+import xg063 from "@/app/sources/images/xg/XG-063.png";
+import xg064 from "@/app/sources/images/xg/XG-064.png";
+import xg065 from "@/app/sources/images/xg/XG-065.png";
+import xg066 from "@/app/sources/images/xg/XG-066.png";
+import xg067 from "@/app/sources/images/xg/XG-067.png";
+import xg068 from "@/app/sources/images/xg/XG-068.png";
+import xg069 from "@/app/sources/images/xg/XG-069.png";
+import xg070 from "@/app/sources/images/xg/XG-070.png";
+import xg071 from "@/app/sources/images/xg/XG-071.png";
+import xg072 from "@/app/sources/images/xg/XG-072.png";
+import xg073 from "@/app/sources/images/xg/XG-073.png";
+import xg074 from "@/app/sources/images/xg/XG-074.png";
+import xg075 from "@/app/sources/images/xg/XG-075.png";
+import xg076 from "@/app/sources/images/xg/XG-076.png";
+import xg077 from "@/app/sources/images/xg/XG-077.png";
+import xg078 from "@/app/sources/images/xg/XG-078.png";
+import xg079 from "@/app/sources/images/xg/XG-079.png";
+import xg080 from "@/app/sources/images/xg/XG-080.png";
+import xg081 from "@/app/sources/images/xg/XG-081.png";
+import xg082 from "@/app/sources/images/xg/XG-082.png";
+import xg083 from "@/app/sources/images/xg/XG-083.png";
+import xg084 from "@/app/sources/images/xg/XG-084.png";
+import xg085 from "@/app/sources/images/xg/XG-085.png";
+import xg086 from "@/app/sources/images/xg/XG-086.png";
+import xg087 from "@/app/sources/images/xg/XG-087.png";
+import xg088 from "@/app/sources/images/xg/XG-088.png";
+import xg089 from "@/app/sources/images/xg/XG-089.png";
+import xg090 from "@/app/sources/images/xg/XG-090.png";
+import xg091 from "@/app/sources/images/xg/XG-091.png";
+import xg092 from "@/app/sources/images/xg/XG-092.png";
+import xg093 from "@/app/sources/images/xg/XG-093.png";
+import xg094 from "@/app/sources/images/xg/XG-094.png";
+import xg095 from "@/app/sources/images/xg/XG-095.png";
+import xg096 from "@/app/sources/images/xg/XG-096.png";
+import xg097 from "@/app/sources/images/xg/XG-097.png";
+import xg098 from "@/app/sources/images/xg/XG-098.png";
+import xg099 from "@/app/sources/images/xg/XG-099.png";
+import xg100 from "@/app/sources/images/xg/XG-100.png";
+import xg101 from "@/app/sources/images/xg/XG-101.png";
+import xg102 from "@/app/sources/images/xg/XG-102.png";
+import xg103 from "@/app/sources/images/xg/XG-103.png";
+import xg104 from "@/app/sources/images/xg/XG-104.png";
+import xg105 from "@/app/sources/images/xg/XG-105.png";
+import xg106 from "@/app/sources/images/xg/XG-106.png";
+import xg107 from "@/app/sources/images/xg/XG-107.png";
+import xg108 from "@/app/sources/images/xg/XG-108.png";
+import xg109 from "@/app/sources/images/xg/XG-109.png";
+import xg110 from "@/app/sources/images/xg/XG-110.png";
+import xg111 from "@/app/sources/images/xg/XG-111.png";
+import xg112 from "@/app/sources/images/xg/XG-112.png";
+import xg113 from "@/app/sources/images/xg/XG-113.png";
+import xg114 from "@/app/sources/images/xg/XG-114.png";
+import xg115 from "@/app/sources/images/xg/XG-115.png";
+import xg116 from "@/app/sources/images/xg/XG-116.png";
+import xg117 from "@/app/sources/images/xg/XG-117.png";
+import xg118 from "@/app/sources/images/xg/XG-118.png";
+import xg119 from "@/app/sources/images/xg/XG-119.png";
+import xg120 from "@/app/sources/images/xg/XG-120.png";
+import xg121 from "@/app/sources/images/xg/XG-121.png";
+import xg122 from "@/app/sources/images/xg/XG-122.png";
 
 export type Product = {
   id: string;
@@ -230,6 +343,119 @@ const xgImages = [
   ["XG-007", xg007],
   ["XG-008", xg008],
   ["XG-009", xg009],
+  ["XG-010", xg010],
+  ["XG-011", xg011],
+  ["XG-012", xg012],
+  ["XG-013", xg013],
+  ["XG-014", xg014],
+  ["XG-015", xg015],
+  ["XG-016", xg016],
+  ["XG-017", xg017],
+  ["XG-018", xg018],
+  ["XG-019", xg019],
+  ["XG-020", xg020],
+  ["XG-021", xg021],
+  ["XG-022", xg022],
+  ["XG-023", xg023],
+  ["XG-024", xg024],
+  ["XG-025", xg025],
+  ["XG-026", xg026],
+  ["XG-027", xg027],
+  ["XG-028", xg028],
+  ["XG-029", xg029],
+  ["XG-030", xg030],
+  ["XG-031", xg031],
+  ["XG-032", xg032],
+  ["XG-033", xg033],
+  ["XG-034", xg034],
+  ["XG-035", xg035],
+  ["XG-036", xg036],
+  ["XG-037", xg037],
+  ["XG-038", xg038],
+  ["XG-039", xg039],
+  ["XG-040", xg040],
+  ["XG-041", xg041],
+  ["XG-042", xg042],
+  ["XG-043", xg043],
+  ["XG-044", xg044],
+  ["XG-045", xg045],
+  ["XG-046", xg046],
+  ["XG-047", xg047],
+  ["XG-048", xg048],
+  ["XG-049", xg049],
+  ["XG-050", xg050],
+  ["XG-051", xg051],
+  ["XG-052", xg052],
+  ["XG-053", xg053],
+  ["XG-054", xg054],
+  ["XG-055", xg055],
+  ["XG-056", xg056],
+  ["XG-057", xg057],
+  ["XG-058", xg058],
+  ["XG-059", xg059],
+  ["XG-060", xg060],
+  ["XG-061", xg061],
+  ["XG-062", xg062],
+  ["XG-063", xg063],
+  ["XG-064", xg064],
+  ["XG-065", xg065],
+  ["XG-066", xg066],
+  ["XG-067", xg067],
+  ["XG-068", xg068],
+  ["XG-069", xg069],
+  ["XG-070", xg070],
+  ["XG-071", xg071],
+  ["XG-072", xg072],
+  ["XG-073", xg073],
+  ["XG-074", xg074],
+  ["XG-075", xg075],
+  ["XG-076", xg076],
+  ["XG-077", xg077],
+  ["XG-078", xg078],
+  ["XG-079", xg079],
+  ["XG-080", xg080],
+  ["XG-081", xg081],
+  ["XG-082", xg082],
+  ["XG-083", xg083],
+  ["XG-084", xg084],
+  ["XG-085", xg085],
+  ["XG-086", xg086],
+  ["XG-087", xg087],
+  ["XG-088", xg088],
+  ["XG-089", xg089],
+  ["XG-090", xg090],
+  ["XG-091", xg091],
+  ["XG-092", xg092],
+  ["XG-093", xg093],
+  ["XG-094", xg094],
+  ["XG-095", xg095],
+  ["XG-096", xg096],
+  ["XG-097", xg097],
+  ["XG-098", xg098],
+  ["XG-099", xg099],
+  ["XG-100", xg100],
+  ["XG-101", xg101],
+  ["XG-102", xg102],
+  ["XG-103", xg103],
+  ["XG-104", xg104],
+  ["XG-105", xg105],
+  ["XG-106", xg106],
+  ["XG-107", xg107],
+  ["XG-108", xg108],
+  ["XG-109", xg109],
+  ["XG-110", xg110],
+  ["XG-111", xg111],
+  ["XG-112", xg112],
+  ["XG-113", xg113],
+  ["XG-114", xg114],
+  ["XG-115", xg115],
+  ["XG-116", xg116],
+  ["XG-117", xg117],
+  ["XG-118", xg118],
+  ["XG-119", xg119],
+  ["XG-120", xg120],
+  ["XG-121", xg121],
+  ["XG-122", xg122],
 ] as const satisfies readonly (readonly [string, StaticImageData])[];
 
 const folderIdByMeasureCode: Record<ProductMeasureCode, ProductFolderId> = {
