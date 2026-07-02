1acd042552d1e85252184f8c506a3aaacee57d03
urrutiarami <urrutiarami@gmail.com>
2026-07-02 14:29:21 -0300

    #

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,10 +52,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="es"
-      className="h-full antialiased"
-    >
+    <html lang="es" className="h-full antialiased">
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
